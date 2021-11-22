453b7811e462f9acfa72495b1bf02d711fc1f238
RicardoSantos-99 <ricardo2010carvalho@hotmail.com.br>
2021-11-22 20:00:29 -0300

    add test

--- test/sum_list_test.exs
+++ test/sum_list_test.exs
@@ -1,8 +1,45 @@
 defmodule SumListTest do
   use ExUnit.Case
-  doctest SumList
 
-  test "greets the world" do
-    assert SumList.hello() == :world
+  describe "call/2" do
+    test "return the list sum" do
+      list = [1, 2, 3, 4, 5]
+
+      response = SumList.call(list)
+
+      expected_response = 15
+
+      assert response == expected_response
+    end
+
+    test "return the list with the negative numbers" do
+      list = [-1, -2, -3, -4, -5]
+
+      response = SumList.call(list)
+
+      expected_response = -15
+
+      assert response == expected_response
+    end
+
+    test "return the list with zero values" do
+      list = [0, 0, 0, 0, 0]
+
+      response = SumList.call(list)
+
+      expected_response = 0
+
+      assert response == expected_response
+    end
+
+    test "return the list with empty list" do
+      list = []
+
+      response = SumList.call(list)
+
+      expected_response = 0
+
+      assert response == expected_response
+    end
   end
 end
