20f7987ec60e98c28c695a4d70639d574dfe1c68
Ricardo Carvalho Santos <ricardo2010carvalho@hotmail.com>
2022-05-12 20:44:42 -0300

    fix: changing functions name

--- lib/enum_functions.ex
+++ lib/enum_functions.ex
@@ -3,14 +3,14 @@ defmodule EnumFunctions do
 
   def smallest(list), do: Enum.min(list)
 
-  def mutiplyFor2(list), do: Enum.map(list, fn value -> value * 2 end)
+  def multiplyFor2(list), do: Enum.map(list, fn value -> value * 2 end)
 
-  def mutiply(list), do: Enum.product(list)
+  def multiply(list), do: Enum.product(list)
 
   def findNumberInList(list, number), do: Enum.find(list, fn value -> value == number end)
 
   def existAnyNumberInListBiggerThanValue(list, value),
     do: Enum.any?(list, fn element -> element > value end)
 
-  def oerderList(list), do: Enum.sort(list)
+  def orderList(list), do: Enum.sort(list)
 end
